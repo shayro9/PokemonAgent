8757e3c0b0c305900aa45d5449e4c1df7a0ee8b1
shayro9 <shayro13@gmail.com>
2026-03-17 22:29:50 +0200

    fixed types saving

--- env/states/pokemon_state.py
+++ env/states/pokemon_state.py
@@ -58,7 +58,7 @@ class PokemonState(ABC):
             self.hp      = pokemon.current_hp_fraction
             self.species = pokemon.species
             self.boosts  = self._encode_boosts(pokemon.boosts, self.BOOST_KEYS)
-            self.types   = self._encode_types(pokemon.types)
+            self.types   = pokemon.types
             self.status  = self._encode_status(pokemon.status)
             self.effects = self._encode_effects(pokemon.effects)
             self.stab    = self._encode_stab(pokemon)
@@ -66,7 +66,7 @@ class PokemonState(ABC):
             self.hp      = 0.0
             self.species = "none"
             self.boosts  = np.zeros(len(self.BOOST_KEYS),  dtype=np.float32)
-            self.types   = np.zeros(len(ALL_TYPES), dtype=np.int32)
+            self.types   = [None]
             self.status  = self._encode_status(None)
             self.effects = self._encode_effects({})
             self.stab    = self._encode_stab(None)
