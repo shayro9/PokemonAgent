b89c7d205495156a9d9bdf60e56629c922386c6a
shayro9 <shayro13@gmail.com>
2026-03-30 22:09:00 +0300

    removed unneeded functionality

--- env/action_mask_gen_1.py
+++ env/action_mask_gen_1.py
@@ -17,15 +17,6 @@ class ActionMaskGen1:
     def get_mask(self) -> np.ndarray:
         return self.mask
 
-    def set_mask(self, battle):
-        available_switches = getattr(battle, "available_switches", [])
-        all_pokemons = getattr(battle, "team", []).values()
-        self._set_mask_range(available_switches, all_pokemons, self.ACTION_SWITCH_RANGE)
-
-        available_moves = getattr(battle, "available_moves", [])
-        all_moves = getattr(battle.active_pokemon, "moves", []).values()
-        self._set_mask_range(available_moves, all_moves, self.ACTION_MOVE_RANGE, move_action=True)
-
     def set(self, mask: list[int]):
         self.mask = np.array(mask, dtype=bool)
 
@@ -82,14 +73,3 @@ class ActionMaskGen1:
         valid_actions = np.where(self.mask)[0]
         lines.append(f"\nValid actions: {valid_actions.tolist()}")
         return "\n".join(lines)
-
-    def _set_mask_range(self, available_items, all_items, action_range, move_action=False):
-        _mask = [item in available_items for item in all_items]
-
-        if not any(_mask) and move_action and len(available_items) > 0:
-            _mask = [1, 0, 0, 0]  # Struggle
-        while len(_mask) < len(action_range):
-            _mask.append(False)
-
-        for slot, action in enumerate(action_range):
-            self.mask[action] = _mask[slot]
